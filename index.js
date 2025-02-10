46131a460b7c1e562c3ad366b179ba88dfb3de1d
sparks042 <chinedufreedom10@gmail.com>
2025-02-10 21:14:22 +0100

    first commit

--- index.js
+++ index.js
@@ -12,11 +12,48 @@ let yearLabel = document.getElementById("yearLabel")
     let today =  new Date();
     console.log(today)
     let yearInput = document.getElementById("year").value
+    let monthInput = document.getElementById('month').value;
+    let dayInput = document.getElementById("day").value;
+    
+
+    // Check if any of the input fields are empty
+    let isDayEmpty = dayInput.value.trim() === "";
+    let isMonthEmpty = monthInput.value.trim() === "";
+    let isYearEmpty = yearInput.value.trim() === "";
+
+    // If any field is empty, set border to red and label font color to red
+    if (isDayEmpty) {
+        dayInput.style.border = "2px solid red";
+        dayLabel.style.color = "red";
+    } else {
+        dayInput.style.border = "";
+        dayLabel.style.color = "";
+    }
+
+    if (isMonthEmpty) {
+        monthInput.style.border = "2px solid red";
+        monthLabel.style.color = "red";
+    } else {
+        monthInput.style.border = "";
+        monthLabel.style.color = "";
+    }
+
+    if (isYearEmpty) {
+        yearInput.style.border = "2px solid red";
+        yearLabel.style.color = "red";
+    } else {
+        yearInput.style.border = "";
+        yearLabel.style.color = "";
+    }
+
+    // check if inputs are empty
+    if (yearInput.trim() === "" || monthInput.trim() === "" || dayInput.trim() === "") {
+        alert("Please enter your date of birth.");
+        return; 
+    }
     let userYear = new Date(yearInput)
     let year = today.getFullYear() - userYear.getFullYear();
-    let monthInput = document.getElementById('month').value;
     let userMonth = new Date(monthInput)
-    let dayInput = document.getElementById("day").value;
     let userDay = new Date(dayInput);
     let monthDiff = today.getMonth() - userMonth.getMonth();
     const data = yearInput +"-" + monthInput + "-" +dayInput 
